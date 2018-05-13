826c745715ff8d411bcafbdd43fe13e07b6992e8
Gridbug <Gridbugkiller@gmail.com>
2018-05-13 20:53:18 +0700

    Added simple view page for Persons db table.

---
+++ ATS_Master.Web/Controllers/PersonsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ATS_Master.Data;
+using ATS_Master.Data.Entities;
+using ATS_Master.Web.Models;
+using Reinforced.Lattice.Configuration;
+using Reinforced.Lattice.Mvc;
+
+namespace ATS_Master.Web.Controllers
+{
+    public class PersonsController : Controller
+    {
+        private readonly ATSContext _context;
+
+        public PersonsController(ATSContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
+        {
+            return View(GenerateViewModel());
+        }
+
+        public PersonsIndexViewModel GenerateViewModel()
+        {
+            return new PersonsIndexViewModel()
+            {
+                Table = new Configurator<Person, PersonRow>()
+                    .Configure()
+                    .Url(Url.Action("HandleTable"))
+            };
+        }
+
+        public ActionResult HandleTable()
+        {
+            var conf = new Configurator<Person, PersonRow>().Configure();
+
+            var handler = conf.CreateMvcHandler(ControllerContext);
+
+            return handler.Handle(_context.Persons);
+        }
+    }
+}
---
+++ ATS_Master.Web/Models/PersonsTable.cs
@@ -0,0 +1,54 @@
+﻿using ATS_Master.Data.Entities;
+using Reinforced.Lattice.Configuration;
+using Reinforced.Lattice.Plugins.Limit;
+using Reinforced.Lattice.Plugins.Paging;
+
+namespace ATS_Master.Web.Models
+{
+    public class PersonRow
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Middlename { get; set; }
+
+        public string Gender { get; set; }
+
+        public int Age { get; set; }
+    }
+
+    public class PersonsIndexViewModel
+    {
+        public Configurator<Person, PersonRow> Table { get; set; }
+    }
+
+    public static class PersonsTable
+    {
+        public static Configurator<Person, PersonRow> Configure(this Configurator<Person, PersonRow> configurator)
+        {
+            configurator.DefaultTable();
+            configurator.OrderFallback(x => x.Id);
+
+            configurator.Column(x => x.Id).DataOnly();
+
+            //configurator.Column(x => x.Gender).Title("Gender");
+
+            return configurator;
+        }
+
+        public static void DefaultTable(this NongenericConfigurator configurator)
+        {
+            configurator.Partition(x => x.InitialSkipTake(take: 10).Client());
+
+            configurator.Limit(ui => ui.Values(new string[] { "10", "20", "50", "-", "All" }), "lt");
+
+            configurator.Paging(ui => ui.PagingWithPeriods().UseGotoPage(), "rb");
+
+            configurator.PrettifyTitles();
+            configurator.AppendEmptyFilters();
+        }
+    }
+}
